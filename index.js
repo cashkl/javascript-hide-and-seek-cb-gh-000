15aaf4451536711014fb45f54b34c80a959dfb47
Murat Celik <cashkl@yandex.com>
2018-05-16 12:55:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function nestedTarget() {
   const a= document.querySelector('#nested .target')
 return a;
 }
+function increaseRankBy(n) {
+  const lis=document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML+=n;
+  }
+}
